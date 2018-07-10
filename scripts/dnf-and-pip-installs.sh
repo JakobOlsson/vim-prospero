670a73e70d531ceeee13ef91bed84068f416a670
Jakob Olsson <jakob@jol-lenovo.localdomain>
2018-07-10 15:19:05 +0200

    now installing nnn file browser

--- scripts/dnf-and-pip-installs.sh
+++ scripts/dnf-and-pip-installs.sh
@@ -2,6 +2,8 @@ dnf -y install vim-enhanced
 dnf -y install vim-command-t
 dnf -y install fish
 dnf -y install tilix
+# nnn - command line file browser URL: https://github.com/jarun/nnn
+dnf -y install nnn
 
 pip install --user --upgrade pip 
 pip install --user awscli
@@ -11,3 +13,6 @@ echo "alias vim=\"vimx\"" >> ~/.bashrc
 echo "alias vi=\"vimx\"" >> ~/.bashrc
 echo "alias vim=\"vimx\"" >> ~/.config/fish/config.fish
 echo "alias vi=\"vimx\"" >> ~/.config/fish/config.fish
+echo "set -x SHELL /usr/bin/fish" >> ~/.config/fish/config.fish
+echo "set -x EDITOR /usr/bin/vimx" >> ~/.config/fish/config.fish
+echo "set -x PAGER /usr/bin/less" >> ~/.config/fish/config.fish
