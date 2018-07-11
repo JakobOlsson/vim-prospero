1b3d9e2ee7a8699e1d3d20c86a8b1d0b2a306973
Jakob Olsson <jakob@jol-lenovo.localdomain>
2018-07-11 11:28:21 +0200

    added source highlight setup to 'less'

--- scripts/dnf-and-pip-installs.sh
+++ scripts/dnf-and-pip-installs.sh
@@ -1,10 +1,17 @@
+# vim with clipboard support
 dnf -y install vim-enhanced
+# nice vim plugin for search/brows/open files <Leader>t
 dnf -y install vim-command-t
+# fish - a nice shell
 dnf -y install fish
+# tiled terminal emulator for gnome
 dnf -y install tilix
 # nnn - command line file browser URL: https://github.com/jarun/nnn
 dnf -y install nnn
+# source highlighting helper - to get source highlight in less
+dnf -y install highlight
 
+# aws development
 pip install --user --upgrade pip 
 pip install --user awscli
 pip install --user boto3
@@ -13,6 +20,11 @@ echo "alias vim=\"vimx\"" >> ~/.bashrc
 echo "alias vi=\"vimx\"" >> ~/.bashrc
 echo "alias vim=\"vimx\"" >> ~/.config/fish/config.fish
 echo "alias vi=\"vimx\"" >> ~/.config/fish/config.fish
+# setup for nnn
 echo "set -x SHELL /usr/bin/fish" >> ~/.config/fish/config.fish
 echo "set -x EDITOR /usr/bin/vimx" >> ~/.config/fish/config.fish
 echo "set -x PAGER /usr/bin/less" >> ~/.config/fish/config.fish
+# setup for source highlight in less
+echo "set -x LESSOPEN \"| /usr/bin/highlight %s --out-format xterm256 --line-numbers --quiet --force --style molokai\"" >> ~/.config/fish/config.fish
+echo "set -x LESSOPEN ' -R '" >> ~/.config/fish/config.fish
+echo "alias more=\"less\"" >> ~/.config/fish/config.fish 
