66d3936eeb9e5f8f5a59162a1d591ee75869dc7f
Jakob Olsson <jakob@jol-lenovo.localdomain>
2018-07-19 13:57:46 +0200

    Added some more dconf keys   fixed minimize and lock screen to not map to movement keys   since it will create moments when you hit the wrong key combo   and lock screen or minimize the window, should just be silently   ignored instead

--- scripts/dconf-settings.sh
+++ scripts/dconf-settings.sh
@@ -13,7 +13,12 @@ dconf write /com/gexperts/Tilix/theme-variant "'dark'"
 
 # gnome shell/mutter keybindings
 # remove Super+H for minimizing
-dconf write org.gnome.desktop.wm.keybindings minimize "[]"
+dconf write /org/gnome/desktop/wm/keybindings/minimize "['']"
+# change lockscreen shortcut
+dconf write /org/gnome/settings-daemon/plugins/media-keys/screensaver "['<Super>q']"
+# if you have "dash to dock" extension instlled -
+# remove mapping that is default to Super+q, which we use above
+dconf write /org/gnome/shell/extensions/dash-to-dock/shortcut-text "''"
 # switch desktop up and down
 dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-up "['<Super>Page_Up', '<Control><Alt>Up', '<Super>k']"
 dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-down "['<Super>Page_Down', '<Control><Alt>Down', '<Super>j']"
