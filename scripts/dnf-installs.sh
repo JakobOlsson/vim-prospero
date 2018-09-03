f078ad0112a234881b8c5e908c31774a01bab273
Jakob Olsson <32457895+JakobOlsson@users.noreply.github.com>
2018-09-03 14:08:57 +0200

    split of install and setup scripts

---
+++ scripts/dnf-installs.sh
@@ -0,0 +1,22 @@
+#!/bin/env sh
+# vim with clipboard support
+sudo dnf -y install vim-enhanced
+sudo dnf -y install vim-X11
+# nice vim plugin for search/brows/open files <Leader>t
+sudo dnf -y install vim-command-t
+# fish - a nice shell
+sudo dnf -y install fish
+# tiled terminal emulator for gnome
+sudo dnf -y install tilix
+# install tmux - tiled terminal
+sudo dnf -y install tmux
+# nnn - command line file browser URL: https://github.com/jarun/nnn
+sudo dnf -y install nnn
+# install xsel for xclipboard tool
+sudo dnf -y install xsel
+# source highlighting helper - to get source highlight in less
+sudo dnf -y install highlight
+# powerline shell statusline
+sudo dnf -y install powerline powerline-fonts
+# dash-to-dock gnome extension
+sudo dnf -y install gnome-shell-extension-dash-to-dock
