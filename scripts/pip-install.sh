4f77b3551ebf18f8f99c09572c67eb9c05fb0176
Jakob Olsson <32457895+JakobOlsson@users.noreply.github.com>
2019-05-01 17:46:38 +0200

    pip newer version

--- scripts/pip-install.sh
+++ scripts/pip-install.sh
@@ -2,17 +2,17 @@
 
 # install python modules
 
-# check that we have the right version of pip
-PIP_VERSION=$(pip --version | awk '{print $2}')
+# check that we have the right version of pip3
+PIP_VERSION=$(pip3 --version | awk '{print $2}')
 echo "Pip version is ${PIP_VERSION}"
-if [ $PIP_VERSION == "18.0" ]; then
+if [ $PIP_VERSION == "19.0.2" ]; then
   # aws development
   echo aws tools
-  #pip install --user --upgrade pip 
-  pip install --user awscli
-  pip install --user boto3
+  #pip3 install --user --upgrade pip3 
+  pip3 install --user awscli
+  pip3 install --user boto3
   echo linters
-  pip install --user flake8
-  pip install --user yamllint
-  pip install --user cfn-lint
+  pip3 install --user flake8
+  pip3 install --user yamllint
+  pip3 install --user cfn-lint
 fi
