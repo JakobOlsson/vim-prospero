e9960ac2c77a0c7f2d1b464b3e9eaa286ff2d9fa
Jakob Olsson <32457895+JakobOlsson@users.noreply.github.com>
2019-06-13 09:29:26 +0200

    git config of aliases

---
+++ scripts/git-setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# set global user name
+git config --global user.name "Jakob Olsson"
+
+# set aliases
+# https://git-scm.com/book/en/v2/Git-Basics-Git-Aliases
+git config --global alias.co checkout
+git config --global alias.b brnahc
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.lg log
+git config --global alias.aa "add -A ."
