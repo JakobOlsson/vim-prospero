9c966441ab981ff025682c518dc3ef79f5188dc0
JakobOlsson <32457895+JakobOlsson@users.noreply.github.com>
2019-12-14 00:00:50 +0100

    steam install 32bits

---
+++ scripts/dnf-steam-install.sh
@@ -0,0 +1,2 @@
+# install steam and 32bit nvidia libs
+sudo dnf -y install steam xorg-x11-drv-nvidia-libs.i686 mesa-libEGL.i686 mesa-libGLES.i686 mesa-libgbm.i686 egl-wayland.i686 libglvnd-egl.i686 libglvnd-gles.i686 libglvnd-opengl.i686 libwayland-server.i686
