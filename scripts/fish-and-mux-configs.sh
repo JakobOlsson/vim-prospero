bff164cd87fa565619251b41ba3fa3b0b4064e12
JakobOlsson <32457895+JakobOlsson@users.noreply.github.com>
2020-06-06 16:13:24 +0200

    remote tmux nested remote tmux, turn of tmux with f12

--- scripts/fish-and-mux-configs.sh
+++ scripts/fish-and-mux-configs.sh
@@ -136,4 +136,32 @@ cat <<'EOF' >> ~/.tmux.conf
 
   set-window-option -g window-status-current-format "#[fg=colour237, bg=colour214, nobold, noitalics, nounderscore]#[fg=colour239, bg=colour214] #I #[fg=colour239, bg=colour214, bold] #W #[fg=colour214, bg=colour237, nobold, noitalics, nounderscore]"
   set-window-option -g window-status-format "#[fg=colour237,bg=colour239,noitalics]#[fg=colour223,bg=colour239] #I #[fg=colour223, bg=colour239] #W #[fg=colour239, bg=colour237, noitalics]"
+
+  # ==============================================
+  # ===   Nesting local and remote sessions     ===
+  # ==============================================
+
+  # Session is considered to be remote when we ssh into host
+
+  # We want to have single prefix key "C-a", usable both for local and remote session
+  # we don't want to "C-a" + "a" approach either
+  # Idea is to turn off all key bindings and prefix handling on local session,
+  # so that all keystrokes are passed to inner/remote session
+
+  # see: toggle on/off all keybindings · Issue #237 · tmux/tmux - https://github.com/tmux/tmux/issues/237
+
+  # Also, change some visual styles when window keys are off
+  bind -T root F12  \
+      set prefix None \;\
+      set key-table off \;\
+      if -F '#{pane_in_mode}' 'send-keys -X cancel' \;\
+      refresh-client -S \;\
+
+  bind -T off F12 \
+    set -u prefix \;\
+    set -u key-table \;\
+    set -u status-style \;\
+    set -u window-status-current-style \;\
+    set -u window-status-current-format \;\
+    refresh-client -S
 EOF
